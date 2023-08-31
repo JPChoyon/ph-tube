94b5ce2df4158828c9046512ee8c590763c1ee0b
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-09-01 00:37:42 +0600

    verified done

--- script/app.js
+++ script/app.js
@@ -23,6 +23,9 @@ const showVideo = (video) => {
     // show card data 
     video.forEach((video) => {
         console.log(video)
+        const verifiedImage = video?.authors[0]?.verified === true
+  ? '<img src="../assest/images/fi_10629607.svg" alt="Verified" />'
+  : '';
         // create element for the card data 
         const videoCard = document.createElement('div');
         cardCon.classList = `card card-compact bg-base-100 shadow-xl grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6`;
@@ -34,18 +37,17 @@ const showVideo = (video) => {
           />
         </figure>
         <div class="card-body ">
-            <div class="flex gap-1">
+            <div class="flex gap-2">
             <div>
             <img class="inline-block rounded-full h-9 w-9" src="${video?.authors[0]?.profile_picture}" alt="">
         </div>
 
         <div>
-            <h2 class="card-title">${video.title}</h2>
-
-                <p class="text-sm font-medium text-gray-700 group-hover:text-neutral-600">${video?.authors[0]?.profile_name} <span> </span></p>
-            
-        
-            <p>${video?.others?.views} views</p>
+            <h2 class="card-title font-bold">${video.title}</h2>
+            <p class="text-sm font-medium flex gap-2 mt-2 text-gray-700 group-hover:text-neutral-600">
+            ${video?.authors[0]?.profile_name} <span>${verifiedImage}</span>
+          </p>  
+            <p class="mt-2">${video?.others?.views} views</p>
         </div>
                 
             </div>
@@ -55,4 +57,6 @@ const showVideo = (video) => {
         cardCon.appendChild(videoCard);
 
     });
+    
 };
+
