400ffa68fa6795f85ff23a103a8cd13bee82a18f
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-09-02 13:58:14 +0600

    added timer section

--- script/app.js
+++ script/app.js
@@ -7,6 +7,17 @@ const showCatagoryData = async () => {
     console.log(cat)
     showCatagory(cat)
 }
+const convertMinutesToHoursAndMinutes = (totalMinutesString) => {
+    const totalMinutes = parseInt(totalMinutesString);
+    if (isNaN(totalMinutes)) {
+      return "";
+    }
+  
+    const hours = Math.floor(totalMinutes / 3600);
+    const minutes = Math.floor((totalMinutes % 3600) / 60);
+  
+    return `${hours} Hrs ${minutes} min ago`;
+  };
 showCatagoryData()
 
 const showCatagory = (cat) => {
@@ -22,7 +33,7 @@ const showCatagory = (cat) => {
         // create element for the card data 
     const div = document.createElement('div');
     div.innerHTML = `
-    <a onclick="handleVideoLoad('${cat.category_id}')" class="tab hover:bg-red-400  active:bg-red-400 bg-gray-200 rounded text-black">${cat.category}</a> 
+    <a onclick="handleVideoLoad('${cat.category_id}')" class="tab active hover:bg-red-400  active:bg-red-400 bg-gray-200 rounded text-black">${cat.category}</a> 
     `;
     catCon.appendChild(div);
     })
@@ -32,10 +43,7 @@ const handleVideoLoad = async (cat = 1000) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${cat}`);
     const data = await res.json();
     videos = data.data
-    console.log(videos)
     showVideo(videos)
-    
-    
 }
 
 // show the video in the card 
@@ -83,53 +91,44 @@ const showVideo = (video) => {
 
         // show card data 
         video.forEach((video) => {
-            console.log(video)
-
             // verifiedImage condition cheack 
 
             const verifiedImage = video?.authors[0]?.verified === true
                 ? '<img src="../assest/images/fi_10629607.svg" alt="Verified" />'
                 : '';
 
-                // Convert the video duration to hours, minutes, and seconds
-        const durationInSeconds = video.others.posted_date;
-        const hours = Math.floor(durationInSeconds / 3600);
-        const minutes = Math.floor((durationInSeconds % 3600) / 60);
-        const seconds = durationInSeconds % 60;
-
-        // verifiedImage condition cheack 
-
-        // const showTime = video?.authors[0]?.posted_date === typeof Number
-        // ? `${hours}h ${minutes}m`
-        // : '';
-
             // create element for the card data 
             const videoCard = document.createElement('div');
-            cardCon.classList = `card card-compact bg-base-100 shadow-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6`;
+            cardCon.classList = `card card-compact bg-base-100 shadow-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-1 lg:gap-6`;
 
             videoCard.innerHTML = `
-        <figure class="h-40 w-[100%]">
+            <div>
+            <figure class="h-40">
           <img
             src="${video.thumbnail}"
-            alt="Shoes"
-            <p class="mt-2">${hours}h ${minutes}m ${seconds}s </p> <!-- Display the duration -->
+            alt="video thumbnail"
           />
         </figure>
+            <div class=" absolute">
+            
+            <h2 class = "ml-[250px] md:ml-[220px] lg:ml-[150px] lg:mt[-120px]" font-semibold style="background-color: black;  color: white; width: 155px;  position: relative;  text-align: center; margin-top:-60px">
+              ${convertMinutesToHoursAndMinutes(video?.others?.posted_date)}
+            </h2>
+          </div>
+            </div>
         <div class="card-body ">
             <div class="flex gap-2">
             <div>
             <img class="inline-block rounded-full h-9 w-9" src="${video?.authors[0]?.profile_picture}" alt="">
         </div>
-
         <div>
             <h2 class="card-title font-bold">${video.title}</h2>
             <p class="text-sm font-medium flex gap-2 mt-2 text-gray-700 group-hover:text-neutral-600">
             ${video?.authors[0]?.profile_name} <span>${verifiedImage}</span>
           </p>  
             <p class="mt-2">${video?.others?.views} views</p>
-        </div>
-                
-            </div>
+        </div>   
+         </div>
         </div>
         `
 
