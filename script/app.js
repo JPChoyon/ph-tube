b5670d645c7f45ed6e7121de959f5940a74989ac
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-09-02 10:19:43 +0600

    added drawing no video content

--- script/app.js
+++ script/app.js
@@ -34,24 +34,75 @@ const handleVideoLoad = async (cat = 1000) => {
     videos = data.data
     console.log(videos)
     showVideo(videos)
+    
+    
 }
-handleVideoLoad()
+
 // show the video in the card 
 const showVideo = (video) => {
+
+    if (!videos || videos.length ===0){
+        // get the card container 
+        const cardCon = document.getElementById('no-video');
+        // clear old data 
+        cardCon.innerHTML = "";
+        const noVideoCard = document.createElement('div');
+        noVideoCard.classList = `w-full mx-auto`
+        noVideoCard.innerHTML =`
+        <div>
+     <div class="w-1/2 mx-auto mt-8">
+       <img class="mx-auto" src="./assest/images/Icon.png" alt="no video" />
+       <h4 class="font-black text-4xl text-center">
+         Opps! Sorry There Is No video
+       </h4>
+     </div>
+   </div>
+        `
+        cardCon.appendChild(noVideoCard)
+   }
+   else{
+    const cardCon = document.getElementById('no-video');
+    cardCon.innerHTML = "";
+   }
+
+    const sortButton = document.getElementById('sort-btn');
+    sortButton.addEventListener('click', () => {
+        // Sort the videos array by views in descending order
+        videos.sort((a, b) => b.others.views - a.others.views);
+        console.log(a.others)
+    
+        // Display the sorted videos
+        showVideo(videos);
+    });
         // get the card container 
         const cardCon = document.getElementById('card-container');
         // clear old data 
         cardCon.innerHTML = "";
 
+ 
+
         // show card data 
         video.forEach((video) => {
-            // console.log(video)
+            console.log(video)
 
             // verifiedImage condition cheack 
 
             const verifiedImage = video?.authors[0]?.verified === true
                 ? '<img src="../assest/images/fi_10629607.svg" alt="Verified" />'
                 : '';
+
+                // Convert the video duration to hours, minutes, and seconds
+        const durationInSeconds = video.others.posted_date;
+        const hours = Math.floor(durationInSeconds / 3600);
+        const minutes = Math.floor((durationInSeconds % 3600) / 60);
+        const seconds = durationInSeconds % 60;
+
+        // verifiedImage condition cheack 
+
+        // const showTime = video?.authors[0]?.posted_date === typeof Number
+        // ? `${hours}h ${minutes}m`
+        // : '';
+
             // create element for the card data 
             const videoCard = document.createElement('div');
             cardCon.classList = `card card-compact bg-base-100 shadow-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6`;
@@ -61,6 +112,7 @@ const showVideo = (video) => {
           <img
             src="${video.thumbnail}"
             alt="Shoes"
+            <p class="mt-2">${hours}h ${minutes}m ${seconds}s </p> <!-- Display the duration -->
           />
         </figure>
         <div class="card-body ">
@@ -80,8 +132,11 @@ const showVideo = (video) => {
             </div>
         </div>
         `
+
+        
             // append the div in the card 
             cardCon.appendChild(videoCard);
         });
     };
 
+    handleVideoLoad()
